0b5eafbe7e0b1f5f2f35170d57b81912be76dfe0
t-log <akhil.paul@ymail.com>
2024-11-03 12:17:29 +0530

    reverts breaks existsCustomerWithEmail

--- backend/src/main/java/com/tlog/service/CustomerJDBCDataAccessService.java
+++ backend/src/main/java/com/tlog/service/CustomerJDBCDataAccessService.java
@@ -55,7 +55,7 @@ public class CustomerJDBCDataAccessService implements CustomerDAO {
     public boolean existsCustomerWithEmail(String email) {
         var sql = """
                 SELECT count(id) FROM customer
-                WHERE name = ?
+                WHERE email = ?
                 """;
         Integer result = jdbcTemplate.queryForObject(sql,Integer.class,email);
         return result != null && result>0;
