3203bc3739fb25878c68ddb8d6b75c3b5df27ac3
t-log <akhil.paul@ymail.com>
2025-01-05 14:38:59 +0530

    updates PingPong and cd changes 2

--- backend/src/main/java/com/tlog/PingPongController.java
+++ backend/src/main/java/com/tlog/PingPongController.java
@@ -13,7 +13,7 @@ public class PingPongController {
     }
     @GetMapping("/pong")
     public PingPong getPongPing(){
-        return new PingPong("Pong Ping * 4");
+        return new PingPong("Pong Ping * 5");
     }
 
 
