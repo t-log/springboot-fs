25b45bfa7595504f8abe83e964f760ac109fb67c
t-log <akhil.paul@ymail.com>
2025-01-05 11:37:53 +0530

    changes PingPong 1

--- backend/src/main/java/com/tlog/PingPongController.java
+++ backend/src/main/java/com/tlog/PingPongController.java
@@ -13,7 +13,7 @@ public class PingPongController {
     }
     @GetMapping("/pong")
     public PingPong getPongPing(){
-        return new PingPong("Pong Ping Anu");
+        return new PingPong("Hey Pong Ping");
     }
 
 
